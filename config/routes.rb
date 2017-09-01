e66c144763e49731d97c28ba6f0e8806b5a64ca2
Boughtmanatee5 <seancostellodp@gmail.com>
2017-09-01 14:59:41 -0400

    created homepage splash

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/signup'
+
+  get 'users/login'
+
+  get 'users/new'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get signup" do
+    get users_signup_url
+    assert_response :success
+  end
+
+  test "should get login" do
+    get users_login_url
+    assert_response :success
+  end
+
 end
